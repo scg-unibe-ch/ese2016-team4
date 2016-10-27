3ae1b617f949f5d9c764b49f43f17d815860a3ec
Balthasar <bauz@gmx.net>
2016-10-27 11:35:34 +0200

    specified BidHistory properties

--- baseProject/src/main/java/ch/unibe/ese/team1/model/BidHistory.java
+++ baseProject/src/main/java/ch/unibe/ese/team1/model/BidHistory.java
@@ -1,8 +1,12 @@
 package ch.unibe.ese.team1.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Date;
 
 /** Describes an advertisement that users can place and search for. */
 @Entity
@@ -11,4 +15,41 @@ public class BidHistory {
 	@Id
 	@GeneratedValue
 	private long id;
+
+	@Column(nullable = false)
+	private long userId;
+	
+	@Column(nullable = false)
+	private long adId;
+	
+	@Column(nullable = false)
+	private long bid;
+	
+	@Column(nullable = false)
+	private Date bidTime;
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public long getAdId() {
+		return adId;
+	}
+
+	public void setAdId(long adId) {
+		this.adId = adId;
+	}
+
+	public long getBid() {
+		return bid;
+	}
+	
+	public void setBid(long bid) {
+		assertTrue("Bid must be >0",bid>0);
+		this.bid = bid;
+	}
 }
