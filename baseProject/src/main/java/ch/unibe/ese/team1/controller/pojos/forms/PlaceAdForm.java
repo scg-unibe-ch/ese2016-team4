264d154d2d2faab0ee6a1646780c8f1b82af1dea
Sven Schmid <sven.schmid@students.unibe.ch>
2016-11-16 22:54:04 +0100

    negative values for start offer or rent arent allowed anymore when you place an ad

--- baseProject/src/main/java/ch/unibe/ese/team1/controller/pojos/forms/PlaceAdForm.java
+++ baseProject/src/main/java/ch/unibe/ese/team1/controller/pojos/forms/PlaceAdForm.java
@@ -42,16 +42,16 @@ public class PlaceAdForm {
 	@Pattern(regexp="^$|^(((0[1-9]|[12]\\d|3[01])\\.(0[13578]|1[02])\\.((1[6-9]|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\.(0[13456789]|1[012])\\.((1[6-9]|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\.02\\.((1[6-9]|[2-9]\\d)\\d{2}))|(29\\.02\\.((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$",message = "dd.MM.yyyy")
 	private String moveOutDate;
 
-	// @Min(value = 1, message = "Has to be equal to 1 or more")
+	@Min(value=0, message = "Negative prizes arent allowed")
 	private int prize;
 	
-	// @Min(value = 1, message = "Has to be equal to 1 or more")
+	@Min(value=0, message = "Negative prizes arent allowed")
 	private int prizeOfSale;
 	
-	// @Min(value = 1, message = "Has to be equal to 1 or more")
+	@Min(value=0, message = "Negative startoffers arent allowed")
 	private int startOffer;
 
-	@Min(value = 1, message = "Has to be equal to 1 or more")
+	@Min(value=0, message = "Dont enter a negative digit")
 	private int squareFootage;
 
 	@NotBlank(message = "Required")
