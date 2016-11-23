98d6cd07f9ce6a1d22e165701c815376f0581d83
kevin-meister <kevin.meister@students.unibe.ch>
2016-11-23 14:31:06 +0100

    if you send a message containing a url, it gets linked. If the url is longer than 35 chars, it gets trimmed

--- baseProject/src/main/java/ch/unibe/ese/team1/controller/service/MessageService.java
+++ baseProject/src/main/java/ch/unibe/ese/team1/controller/service/MessageService.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.transaction.Transactional;
 
@@ -30,14 +31,23 @@ public class MessageService {
 	@Autowired
 	private MessageDao messageDao;
 
-	/** Gets all messages in the inbox of the given user, sorted newest to oldest */
+	// Pattern for recognizing a URL, based off RFC 3986
+	private static final Pattern urlPattern = Pattern.compile(
+			//"(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)" + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+			//		+ "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",
+			"(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)" + ".*",
+			Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+
+	/**
+	 * Gets all messages in the inbox of the given user, sorted newest to oldest
+	 */
 	@Transactional
 	public Iterable<Message> getInboxForUser(User user) {
 		Iterable<Message> usersMessages = messageDao.findByRecipient(user);
 		List<Message> messages = new ArrayList<Message>();
-		for(Message message: usersMessages)
+		for (Message message : usersMessages)
 			messages.add(message);
-		Collections.sort(messages, new Comparator<Message>(){
+		Collections.sort(messages, new Comparator<Message>() {
 			@Override
 			public int compare(Message message1, Message message2) {
 				return message2.getDateSent().compareTo(message1.getDateSent());
@@ -72,15 +82,15 @@ public class MessageService {
 
 		message.setRecipient(userDao.findByUsername(messageForm.getRecipient()));
 		message.setSubject(messageForm.getSubject());
-		message.setText(messageForm.getText());
+		message.setText(getUrl(messageForm.getText()));
 		message.setState(MessageState.UNREAD);
-		
+
 		// get logged in user as the sender
 		org.springframework.security.core.userdetails.User securityUser = (org.springframework.security.core.userdetails.User) SecurityContextHolder
 				.getContext().getAuthentication().getPrincipal();
 
 		User loggedInUser = userDao.findByUsername(securityUser.getUsername());
-		
+
 		message.setSender(loggedInUser);
 
 		Calendar calendar = Calendar.getInstance();
@@ -94,28 +104,58 @@ public class MessageService {
 		return message;
 	}
 
-	/** Saves a new message with the given parameters in the DB.
+	/**
+	 * Saves a new message with the given parameters in the DB.
 	 * 
-	 * @param sender the user who sends the message
-	 * @param recipient the user who should receive the message
-	 * @param subject the subject of the message
-	 * @param text the text of the message
+	 * @param sender
+	 *            the user who sends the message
+	 * @param recipient
+	 *            the user who should receive the message
+	 * @param subject
+	 *            the subject of the message
+	 * @param text
+	 *            the text of the message
 	 */
-	public void sendMessage(User sender, User recipient, String subject,
-			String text) {
+	public void sendMessage(User sender, User recipient, String subject, String text) {
 		Message message = new Message();
 		message.setDateSent(new Date());
 		message.setSender(sender);
 		message.setRecipient(recipient);
 		message.setSubject(subject);
-		message.setText(text);
+		message.setText(getUrl(text));
+
 		message.setState(MessageState.UNREAD);
-		
+
 		messageDao.save(message);
+	}
+
+	public String getUrl(String text) {
+
+		// separate input by spaces (since URLs don't have spaces... )
+		// if a link gets copied without a space (e.g. check this out:www.google.ch) it doesn't work
+		String[] parts = text.split("\\s+");
+		String linkedText = "";
+		// get every part
+		for (String item : parts) {
+			if (urlPattern.matcher(item).matches()) {
+				// it's a url, so we link it and concatenate it after
+				String name = item;
+				if (item.length()>35){
+					name = item.substring(0, 35);
+					name = name.concat("...");
+				}
+				linkedText = linkedText.concat("<a href=\"" + item + "\">" + name + "</a> ");
+			} else {
+				// no url, so normal concatenating
+				linkedText = linkedText.concat(item + " ");
+			}
+		}
+		return linkedText;
 	}
 
 	/**
 	 * Sets the MessageState of a given Message to "READ".
+	 * 
 	 * @param id
 	 */
 	@Transactional
@@ -124,15 +164,15 @@ public class MessageService {
 		message.setState(MessageState.READ);
 		messageDao.save(message);
 	}
-	
+
 	/** Returns the number of unread messages a user has. */
 	@Transactional
 	public int unread(long id) {
 		User user = userDao.findOne(id);
 		Iterable<Message> usersMessages = messageDao.findByRecipient(user);
 		int i = 0;
-		for(Message message: usersMessages) {
-			if(message.getState().equals(MessageState.UNREAD))
+		for (Message message : usersMessages) {
+			if (message.getState().equals(MessageState.UNREAD))
 				i++;
 		}
 		return i;
