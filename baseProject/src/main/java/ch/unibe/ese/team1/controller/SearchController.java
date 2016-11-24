79b66e0e0473c84cbf359a126fdfedfe9a21959b
Sven Schmid <sven.schmid@students.unibe.ch>
2016-11-24 15:52:18 +0100

    added searchform for index page but needs review

--- baseProject/src/main/java/ch/unibe/ese/team1/controller/SearchController.java
+++ baseProject/src/main/java/ch/unibe/ese/team1/controller/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ch.unibe.ese.team1.controller.pojos.forms.SearchForm;
@@ -43,11 +44,48 @@ public class SearchController {
 		return model;
 	}
 
+	
+	@RequestMapping(value="/indexSearch", method = {RequestMethod.POST})
+	public ModelAndView indexSearch(@RequestParam("city") String city,
+			@Valid SearchForm searchForm,
+			BindingResult result){
+		searchForm.deleteLists();
+		searchForm.setCity(city);
+		searchForm.setPrize(1000000);
+		
+		searchForm.setFlat(searchForm.getFlat());
+		searchForm.setHouse(searchForm.getHouse());
+		searchForm.setStudio(searchForm.getStudio());
+		searchForm.setRoom(searchForm.getRoom());
+		searchForm.setRent(true);
+		searchForm.setAuction(true);
+		searchForm.setBuy(true);
+		searchForm.setRadius(2000);
+		
+		if (!result.hasErrors()) {
+			ModelAndView model = new ModelAndView("results");
+			Iterable<Ad> searchFormResults = adService.queryResults(searchForm);
+			Iterable<String> googleMapCoords = adService.findCoords(searchFormResults);
+			model.addObject("results", searchFormResults);
+			model.addObject("coords", googleMapCoords);
+			//bad, only temporary fix
+			model.addObject("bidService", bidService);
+			searchForm.deleteLists();
+			return model;
+		} else {
+			// go back
+			ModelAndView model = new ModelAndView("index");
+			model.addObject("newest", adService.getNewestAds(4));
+			model.addObject("bidService", bidService);
+			return model;
+		}
+	}
+
 	/**
 	 * Gets the results when filtering the ads in the database by the parameters
 	 * in the search form.
 	 */
-	@RequestMapping(value = "/results", method = { RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/results", method = {RequestMethod.POST})
 	public ModelAndView results(@Valid SearchForm searchForm,
 			BindingResult result) {
 		if (!result.hasErrors()) {
